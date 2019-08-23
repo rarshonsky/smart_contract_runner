460fbf7328021c481a8d3dccfa0c2db261ada0af
rarshonsky <robert.arshonsky@gmail.com>
2019-08-23 00:45:36 -0700

    Adds styling

--- client/src/App.js
+++ client/src/App.js
@@ -86,7 +86,6 @@ class App extends Component {
           console.log("Error");
           throw error;
       }else{
-          console.log(result);
           var parse_abi = JSON.parse(result);
           let new_contract = this.state.web3.eth.contract(parse_abi).at(this.state.contractAddress)
           this.setState({
@@ -139,6 +138,35 @@ class App extends Component {
           <Segment>
             <h2>No connection to Thunder</h2>
             <p>Please make sure you are using a DApp browser such as MetaMask or Mist.</p>
+            <p> This contract is deployed to the Thundercore Test Net</p>
+            <p> Thundercore Wallet Instructions: <a href='https://www.thundercore.com/wallet-instructions'>https://www.thundercore.com/wallet-instructions</a> </p>
+            <p> Add custom RPC on MetaMask withthe following values:
+
+
+          </p>
+          <table>
+            <tr>
+              <td>Network Name </td>
+              <td> Thundercore Test Net </td>
+            </tr>
+            <tr>
+              <td>New RPC URL</td>
+              <td> https://testnet-rpc.thundercore.com </td>
+            </tr>
+            <tr>
+              <td>ChainID </td>
+              <td> 18 </td>
+            </tr>
+            <tr>
+              <td>Symbol </td>
+              <td> TT </td>
+            </tr>
+          </table>
+
+          <p> Once connected Go to <a href='https://faucet-testnet.thundercore.com'>https://faucet-testnet.thundercore.com</a> to get free testnet tokens.</p>
+          <p>Authenticate with Google or Github. </p>
+          <p>Enter Wallet address to receive tokens at (this can be found at the top of MetaMask, click Account 1 and it'll copy the address) </p>
+
           </Segment>
         )
       }
@@ -146,16 +174,23 @@ class App extends Component {
 
     return (
       <div id="main" className="">
-        <Header as='h1'>Smart Contract Runner </Header>
+        <Header as='h1' className="name">Smart Contract Runner</Header>
+        <Header as='h3'>
+          <a href="https://github.com/rarshonsky/smart_contract_runner"><svg width="50" height="50" class="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg></a>
+
+        </Header>
+        <Header as='h5' className='white'>
+            <a href="https://dkey.store">Decentralized Key Store</a> test contract address: 0x574145d19c3e33518dE2c95A947182901E2Ad021
+        </Header>
         <Divider />
         <div>
           <Segment>
             <Form onSubmit={this.handleContract.bind(this)}>
               <Form.Field>
                 <label>Contract Address</label>
-                <input value={this.state.contractAddress} onChange={this.handleChange.bind(this)} data-prop-name= 'contractAddress'/>
+                <input id="contract-address" value={this.state.contractAddress} onChange={this.handleChange.bind(this)} data-prop-name= 'contractAddress'/>
               </Form.Field>
-              <Button type="submit" className="btn btn-info btn-lg" data-toggle="modal" data-target="#contractResults">Get Contract!</Button>
+              <Button id="contract-button" type="submit" className="btn btn-info btn-lg" data-toggle="modal" data-target="#contractResults">Get Contract!</Button>
             </Form>
           </Segment>
         </div>
@@ -172,7 +207,6 @@ class App extends Component {
                     Contract Call Results
                     <textarea rows="20" cols="100" value={this.state.contract_result || ''} disabled />
                   </label>
-
                    <div id="display-data-Container">
                    <form onSubmit={this.handleContractCall.bind(this)}>
                        <select onChange={this.handleSelectChange.bind(this)}>
